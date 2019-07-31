2b2454dad501176248b60aeec329df2d900aeb64
mdhamija <mdhamija94@gmail.com>
2019-07-30 17:56:00 -0700

    Initial commit for W6D2

---
+++ active_record_lite/lib/01_sql_object.rb
@@ -0,0 +1,75 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @columns if @columns
+    cols = DBConnection.execute(<<-SQL).first
+      select
+        *
+      from
+        #{self.table_name}
+      limit
+        0
+    SQL
+    # p results
+    cols.map! { |el| el.to_sym }
+    # p results
+    @cols = cols
+  end
+
+  def self.finalize!
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.name.tableize
+  end
+
+  def self.all
+    # ...
+    results = DBConnection.execute(<<-SQL)
+      select
+        #{table_name}.*
+      from
+        #{table_name}
+    SQL
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
